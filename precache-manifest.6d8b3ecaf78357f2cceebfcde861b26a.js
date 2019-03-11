e5e5feaaa3dab4844c0a92d4f00c4e37eed60270
Hari Kiran Yalavarthi <harikiranyalavarthi@qasymphony.com>
2019-03-11 12:01:09 -0400

    Updates

---
+++ precache-manifest.6d8b3ecaf78357f2cceebfcde861b26a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ChatApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "10535b7305eede8feba3",
+    "url": "/ChatApp/static/js/runtime~main.805a290e.js"
+  },
+  {
+    "revision": "d10c9296901a283ce456",
+    "url": "/ChatApp/static/js/main.f31cb220.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/ChatApp/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "d10c9296901a283ce456",
+    "url": "/ChatApp/static/css/main.738438c2.chunk.css"
+  },
+  {
+    "revision": "58508baa01810e18ae8c571d52e28315",
+    "url": "/ChatApp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/chatapp_v2/precache-manifest.5abe0474ebbc41610cffb9bf2d522ee5.js"
+  "/ChatApp/precache-manifest.6d8b3ecaf78357f2cceebfcde861b26a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/chatapp_v2/index.html", {
+workbox.routing.registerNavigationRoute("/ChatApp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
