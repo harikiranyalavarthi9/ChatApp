588c78761fce0c7ea3d012c611f9bcfbcbd9375e
Hari Kiran Yalavarthi <harikiranyalavarthi@qasymphony.com>
2019-07-25 18:04:08 -0400

    Added sample html pages instead of pug

--- v2/backend/routes/index.js
+++ v2/backend/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.html');
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about.html');
+});
+
+router.get('/sitemap', function(req, res, next) {
+  res.render('sitemap.html');
 });
 
 module.exports = router;
--- v2/backend/routes/users.js
+++ v2/backend/routes/users.js
@@ -14,16 +14,25 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:id', function(req, res, next) {
-  res.json([{
+  const family_data = [{
+    id: 1,
     firstname: "Hari Kiran",
     lastname: "Yalavarthi"
   }, {
+    id: 2,
     firstname: "Ravindra Babu",
     lastname: "Yalavarthi"
   }, {
+    id: 3,
     firstname: "Vanaja",
     lastname: "Gogineni"
-  }]);
+  },
+  {
+    id: 4,
+    firstname: "Vinaya Sai",
+    lastname: "Yalavarthi"
+  }];
+  res.json(family_data.filter(obj => obj.id == req.params.id));
 });
 
 module.exports = router;
